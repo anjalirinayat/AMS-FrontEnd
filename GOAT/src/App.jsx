010f6053cc79a773c6242042950b137bc1584430
vaishu-dd45 <vaishnvirahangdale123@gmail.com>
2025-10-16 08:30:01 +0530

    nj

--- GOAT/src/App.jsx
+++ GOAT/src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
       <Routes>
-        {/* add routes here */}
+        <Route path="/" element={<Home />} />
       </Routes>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ GOAT/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Helo() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default Helo;
