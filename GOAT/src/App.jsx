e8ab421ca455c79d190624523d94c2903e3d0a80
Anjali Rinayat <anjalirinayat52@gmail.com>
2025-10-16 23:32:36 +0530

    added logo to asset

--- GOAT/src/App.jsx
+++ GOAT/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Routes>
-        {/* add routes here */}
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   )
 }
